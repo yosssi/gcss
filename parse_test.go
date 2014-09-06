14528a9461a3174dfd2014b3c3d034b6e49f5053
yosssi <yoshida.keiji.84@gmail.com>
2014-09-06 23:20:12 +0900

    Update parse_test.go

--- parse_test.go
+++ parse_test.go
@@ -19,3 +19,12 @@ func Test_parse(t *testing.T) {
 		t.Errorf("error occurred [error: %s]", err.Error())
 	}
 }
+
+func Test_formatLF(t *testing.T) {
+	s := cr + crlf + lf + "a" + crlf + lf + cr + "b" + lf + cr + crlf
+	expectedS := lf + lf + lf + "a" + lf + lf + lf + "b" + lf + lf + lf
+
+	if formatLF(s) != expectedS {
+		t.Errorf("formatLF(s) should be %s [actual: %s]", expectedS, formatLF(s))
+	}
+}
